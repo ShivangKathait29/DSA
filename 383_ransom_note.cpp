96057bd3bf5b50a303cdae6f46f727708b1233a4
Shivang Kathait <shivangkathait29@gmail.com>
2026-08-05 15:27:40 +0530

    Add 383_ransom_note.cpp - LeetCode Problem 383

---
+++ 383_ransom_note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(char c : magazine){
+            mp[c]++;
+        }
+        for(char c : ransomNote){
+            if(mp[c]==0) mp.erase(c);
+            if(mp.count(c)){
+                mp[c]--;  
+            }else return false;
+        }
+        return true;
+    }
+};
