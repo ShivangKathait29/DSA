4cc463673fe02423c9de684860e9c473d70ae28b
Shivang Kathait <shivangkathait29@gmail.com>
2026-08-11 19:02:25 +0530

    Add 2050_count_good_numbers.cpp - LeetCode Problem 2050

---
+++ 2050_count_good_numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    long long rec(long long base,long long power){
+        long long result = 1;
+        base %= MOD;
+        while(power>0){
+            if(power%2==1){
+                result = (result * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            power /=2;
+        }
+        return result;
+    }
+    int countGoodNumbers(long long n) {
+        long long e = (n+1)/2;
+        long long o = n/2;
+        long long evenWays = rec(5,e);
+        long long oddWays = rec(4,o);
+        return (evenWays * oddWays) % MOD;
+    }
+};
