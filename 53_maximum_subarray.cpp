dd352d38b49ab04db9aef15fee9f4d8135f1c732
Shivang Kathait <shivangkathait29@gmail.com>
2026-08-12 17:06:54 +0530

    Update 53_maximum_subarray.cpp - LeetCode Problem 53

--- 53_maximum_subarray.cpp
+++ 53_maximum_subarray.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int sum = INT_MIN;
-        int currsum = 0;
-        if(nums.size()==1) return nums[0];
-        for(int i=0;i<nums.size();i++){
-            currsum+=nums[i];
-            if(currsum > sum) sum = currsum;
-            if(currsum<0) currsum = 0;
+    int sub(vector<int>& nums,int low,int high){
+        if(low==high) return nums[low];
+        int mid = low + (high - low)/2;
+
+        int left = sub(nums,low,mid);
+        int right = sub(nums,mid+1,high);
+
+        int ls = INT_MIN;
+        int leftSum = 0;
+
+        for(int i=mid;i>=low;i--){
+            leftSum+=nums[i];
+            ls = max(ls,leftSum);
         }
-        return sum;
+
+        int rs = INT_MIN;
+        int rightSum = 0;
+
+        for(int i=mid+1;i<=high;i++){
+            rightSum += nums[i];
+            rs = max(rs,rightSum);
+        }
+
+        int cross = ls + rs;
+
+        return max({left,cross,right});
+    }
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int low=0,high=n-1;
+        return sub(nums,low,high);
     }
 };
