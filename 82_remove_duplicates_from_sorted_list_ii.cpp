87502207e0609e0c9f112697a5baaad082625e02
Shivang Kathait <shivangkathait29@gmail.com>
2026-08-25 16:30:12 +0530

    Add 82_remove_duplicates_from_sorted_list_ii.cpp - LeetCode Problem 82

---
+++ 82_remove_duplicates_from_sorted_list_ii.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+        if(!head || !head->next) return head;
+        
+        if(head->val == head->next->val){
+            int duplicate = head->val;
+            while(head && head->val == duplicate){
+                head = head->next;
+            }
+            return deleteDuplicates(head);
+            
+        }
+        head->next = deleteDuplicates(head->next);
+        return head;
+        
+    }
+};
